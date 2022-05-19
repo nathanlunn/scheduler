cf4d0b6c532a19903e4cd74455eac4747623f421
Summer <user@users-Air.local.tld>
2022-05-19 09:56:59 -0400

    added appointment functionality

--- stories/index.js
+++ stories/index.js
@@ -9,6 +9,15 @@ import Button from 'components/Button';
 import DayListItem from 'components/DayListItem';
 import DayList from 'components/DayList';
 import InterviewerListItem from 'components/InterviewerListItem';
+import InterviewerList from 'components/InterviewerList';
+import Appointment from "components/Appointment/index";
+import Header from 'components/Appointment/Header';
+import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
+import Status from 'components/Appointment/Status';
+import Error from 'components/Appointment/Error';
+
 
 storiesOf('Button', module)
   .addParameters({
@@ -55,18 +64,18 @@ const days = [
   },
 ];
 
-storiesOf('DayList', module)
+storiesOf("DayList", module)
   .addParameters({
-    backgrounds: [{ name: 'dark', value: '#222f3e', default: true }],
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
   })
-  .add('Monday', () => (
-    <DayList days={days} day={'Monday'} setDay={action('setDay')} />
+  .add("Monday", () => (
+    <DayList days={days} value={"Monday"} onChange={action("setDay")} />
   ))
-  .add('Tuesday', () => (
-    <DayList days={days} day={'Tuesday'} setDay={action('setDay')} />
+  .add("Tuesday", () => (
+    <DayList days={days} value={"Tuesday"} onChange={action("setDay")} />
   ))
-  .add('Wednesday', () => (
-      <DayList days={days} day={'Wednesday'} setDay={action('setDay')} />
+  .add("Wednesday", () => (
+    <DayList days={days} value={"Wednesday"} onChange={action("setDay")} />
   ));
 
 const interviewer = {
@@ -102,3 +111,66 @@ storiesOf("InterviewerListItem", module)
       setInterviewer={action("setInterviewer")}
     />
   ));
+
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
+
+storiesOf("InterviewerList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Initial", () => (
+    <InterviewerList
+      interviewers={interviewers}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerList
+      interviewers={interviewers}
+      value={3}
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerList
+      interviewers={interviewers}
+      onChange={action("setInterviewer")}
+    />
+  ));
+
+storiesOf('Appointment', module)
+  .addParameters({
+    backgrounds: [{ name: 'white', value: '#fff', default: true }]
+  })
+  .add('Appointment', () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time={'12pm'} />)
+  .add('Empty', () => <Empty onAdd={action('onAdd')} />)
+  .add('Show', () => (
+    <Show
+      student={'Lydia Miller-Jones'}
+      interviewer={interviewers[0].name}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+    />
+  ))
+  .add('Confirm', () => (
+    <Confirm
+      message={'Delete the Appointment?'}
+      onConfirm={action('onConfirm')}
+      onCancel={action('onCancel')}
+    />
+  ))
+  .add('Status', () => (
+    <Status message={'Deleting'} />
+  ))
+  .add('Error', () => (
+    <Error
+      message={'Could not delete the appointment.'}
+      onClose={action('onClose')}
+    />
+  ))
